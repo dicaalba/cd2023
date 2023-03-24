af925863bb4984180f3ec7843302668deba30bc0
Diana Alfaro <dicaalba@gmail.com>
2023-03-24 17:38:08 -0500

    feat(cd2023): wip

---
+++ build/precache-manifest.849636f71809a80b82aebd85ec0d9366.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239c540056fd7adeba5fc45f05da43b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b8fbe215f2830956967",
+    "url": "/static/css/main.6324ea5f.chunk.css"
+  },
+  {
+    "revision": "704fd1872a70051a4832",
+    "url": "/static/js/2.de4973d8.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.de4973d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b8fbe215f2830956967",
+    "url": "/static/js/main.209dd97e.chunk.js"
+  },
+  {
+    "revision": "efc93394c52cf1132811",
+    "url": "/static/js/runtime-main.9eb0259d.js"
+  },
+  {
+    "revision": "a6da7e7ddb5aba8d029ed1fd9e6e3144",
+    "url": "/static/media/CommunityDay_logo.a6da7e7d.png"
+  },
+  {
+    "revision": "52599040f68eb166fbdc9e854f435a4d",
+    "url": "/static/media/aboutbanner.52599040.png"
+  },
+  {
+    "revision": "d195b0fe6c94cfdf2c7846d1b0fdf16c",
+    "url": "/static/media/alfredo_alva.d195b0fe.png"
+  },
+  {
+    "revision": "c2f9856eb6b15eb269425c4fa023265b",
+    "url": "/static/media/andre_cervantes.c2f9856e.png"
+  },
+  {
+    "revision": "316ece3ad87d354748883f357f51b21d",
+    "url": "/static/media/aws_ugsec_latam.316ece3a.png"
+  },
+  {
+    "revision": "653cc48364e1e40535b89dda1f0e9577",
+    "url": "/static/media/axel_pierola.653cc483.png"
+  },
+  {
+    "revision": "f78ff773b2f30aa658e51eaf211e10ac",
+    "url": "/static/media/banner.f78ff773.png"
+  },
+  {
+    "revision": "a794d397aca697511818408c8e9a69ad",
+    "url": "/static/media/blue-cloud.a794d397.svg"
+  },
+  {
+    "revision": "29a6b6d8c47e938440621a43f1dfb1bf",
+    "url": "/static/media/carlos_cortez.29a6b6d8.png"
+  },
+  {
+    "revision": "80fd851ee26ec057ffed98a9c5829bd0",
+    "url": "/static/media/claudia_izquierdo.80fd851e.png"
+  },
+  {
+    "revision": "856f7fedc5724ed1b57e032c76cad935",
+    "url": "/static/media/corrado_daly.856f7fed.png"
+  },
+  {
+    "revision": "f692a4b5e4620885855037859fb716aa",
+    "url": "/static/media/diana_alfaro.f692a4b5.png"
+  },
+  {
+    "revision": "cbe5da10dad8484f9aa2b1d78b0d1134",
+    "url": "/static/media/doris_manrique.cbe5da10.png"
+  },
+  {
+    "revision": "a4f3919fc25beb9e93940d57f98a2582",
+    "url": "/static/media/eli_fuentes.a4f3919f.png"
+  },
+  {
+    "revision": "474c1fc39af193d0baac3bd956298dae",
+    "url": "/static/media/felipe_espinoza.474c1fc3.png"
+  },
+  {
+    "revision": "2a12599c48aad7aaa332bdd1ad1b6221",
+    "url": "/static/media/fernando_gamero.2a12599c.png"
+  },
+  {
+    "revision": "38d1e6ee97f456916c0bd9a4b7a5364e",
+    "url": "/static/media/festive-img.38d1e6ee.png"
+  },
+  {
+    "revision": "759dc92c97758c98fd1b3028367445ee",
+    "url": "/static/media/gallery1.759dc92c.png"
+  },
+  {
+    "revision": "77594a722d6e49bcef0951d44e91470c",
+    "url": "/static/media/gallery2.77594a72.png"
+  },
+  {
+    "revision": "8e76fae26b6c4f946f4d3df1a920d6b8",
+    "url": "/static/media/gallery3.8e76fae2.png"
+  },
+  {
+    "revision": "7e5e902e80683657d16a138858604169",
+    "url": "/static/media/gallery4.7e5e902e.png"
+  },
+  {
+    "revision": "3cc6f44a338fb7013aa940c5c2aa1414",
+    "url": "/static/media/gallery5.3cc6f44a.png"
+  },
+  {
+    "revision": "a5c586c6a238c339a7e05e2f79ba2ae0",
+    "url": "/static/media/gallery6.a5c586c6.png"
+  },
+  {
+    "revision": "60d682298d026eb319e587be7d988231",
+    "url": "/static/media/gerardo_castro.60d68229.png"
+  },
+  {
+    "revision": "709fc581c43db9124694ce0cfc739fa6",
+    "url": "/static/media/grey-cloud.709fc581.svg"
+  },
+  {
+    "revision": "976b89e5fcd0ff78873ce201bb778a54",
+    "url": "/static/media/ivan_echegaray.976b89e5.png"
+  },
+  {
+    "revision": "86a589558de95e837711acb49085174f",
+    "url": "/static/media/joe_huamani.86a58955.png"
+  },
+  {
+    "revision": "1bf800fe7885c5bae89b327c960a55ee",
+    "url": "/static/media/jose_yapur.1bf800fe.png"
+  },
+  {
+    "revision": "119430027b589156bb03747f327e475f",
+    "url": "/static/media/julian_arango.11943002.png"
+  },
+  {
+    "revision": "a2210658fe12d34fb7bfdc2593ba6981",
+    "url": "/static/media/julio_mendoza.a2210658.png"
+  },
+  {
+    "revision": "f76f04785987d5394392ed0e7e73a161",
+    "url": "/static/media/magali_pinto.f76f0478.png"
+  },
+  {
+    "revision": "190529f5d8f0d8cb9ea49173cd820e6c",
+    "url": "/static/media/mapa.190529f5.png"
+  },
+  {
+    "revision": "a84912c5b0f46675c17ea5bd103b5d67",
+    "url": "/static/media/maricela_miranda.a84912c5.png"
+  },
+  {
+    "revision": "6dc95a2219555ea0c2d8cc7316f0971c",
+    "url": "/static/media/mayerli_romero.6dc95a22.png"
+  },
+  {
+    "revision": "37ff391250cbc7240b07a122bb557894",
+    "url": "/static/media/michael_rodrigo.37ff3912.png"
+  },
+  {
+    "revision": "c10a0e4c70a173a1a2cd7be15fe2be1b",
+    "url": "/static/media/orange-cloud.c10a0e4c.svg"
+  },
+  {
+    "revision": "13f52b3f31c5e66cc3e47fb03daa99fe",
+    "url": "/static/media/organization1.13f52b3f.png"
+  },
+  {
+    "revision": "b6293cba00c955037573b151804d97bf",
+    "url": "/static/media/organization2.b6293cba.png"
+  },
+  {
+    "revision": "d73ff9e71b0fc50a1f1814036e672e00",
+    "url": "/static/media/organization3.d73ff9e7.png"
+  },
+  {
+    "revision": "d798127f2650643b794fbd9603c6e73a",
+    "url": "/static/media/organization4.d798127f.png"
+  },
+  {
+    "revision": "a892c26580ae8417851de0f9d6e8ac96",
+    "url": "/static/media/organization5.a892c265.png"
+  },
+  {
+    "revision": "f021604f22af0b9a8c2352fb04b5fb7a",
+    "url": "/static/media/raul_hugo.f021604f.png"
+  },
+  {
+    "revision": "f8f86f2df403389e555973bcc7f42741",
+    "url": "/static/media/rossana_suarez.f8f86f2d.png"
+  },
+  {
+    "revision": "9fa6f0fbba83cce53887d8d3e1ed43d4",
+    "url": "/static/media/shedulebanner.9fa6f0fb.png"
+  },
+  {
+    "revision": "19ae799893f01ea67c6a6b4b1264f51c",
+    "url": "/static/media/sponsor1.19ae7998.png"
+  },
+  {
+    "revision": "dc191421f87f435de517d48b8571819c",
+    "url": "/static/media/sponsor10.dc191421.png"
+  },
+  {
+    "revision": "d0b535e1ca95ec78b887bea8436283e8",
+    "url": "/static/media/sponsor11.d0b535e1.png"
+  },
+  {
+    "revision": "d076aeb76786646a6e47b99aa098a166",
+    "url": "/static/media/sponsor2.d076aeb7.png"
+  },
+  {
+    "revision": "2d0f5545949969519cac880d81e19c05",
+    "url": "/static/media/sponsor3.2d0f5545.png"
+  },
+  {
+    "revision": "d134500e9d674a3fc0e9ecd3ade38c82",
+    "url": "/static/media/sponsor4.d134500e.png"
+  },
+  {
+    "revision": "dfd1d1a78646f6ec7863d2ebacade28d",
+    "url": "/static/media/sponsor5.dfd1d1a7.png"
+  },
+  {
+    "revision": "795bf3789a6910630a5714b867e8f507",
+    "url": "/static/media/sponsor6.795bf378.png"
+  },
+  {
+    "revision": "5b01e132eb6e3eebd33fdc2e746a1168",
+    "url": "/static/media/sponsor7.5b01e132.png"
+  },
+  {
+    "revision": "5c140149db78f523c0170d6d8004d3a0",
+    "url": "/static/media/sponsor9.5c140149.png"
+  },
+  {
+    "revision": "fe95bbb590261b6bab058c3411846def",
+    "url": "/static/media/topics.fe95bbb5.png"
+  },
+  {
+    "revision": "2f91062f6f170258f652a8de836e66b2",
+    "url": "/static/media/walter_levano.2f91062f.png"
+  },
+  {
+    "revision": "44b5b4d369392cbbaf6be2646f147724",
+    "url": "/static/media/white-cloud.44b5b4d3.svg"
+  }
+]);
